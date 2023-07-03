b0dda4d1c9dee380188c29db107acb0c46ff796d
Richard Ji <102701271+gintoki420@users.noreply.github.com>
2023-07-03 14:08:31 +1000

    Create main.sh
    start with this for now - gpt3

---
+++ main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the paths to the input files
+all_packages_file="all_packages.txt"
+faulty_packages_file="faulty_packages.txt"
+
+# Read all packages into an array
+mapfile -t all_packages < "$all_packages_file"
+
+# Read faulty packages into an associative array
+declare -A faulty_packages
+while IFS= read -r package || [[ -n "$package" ]]; do
+    faulty_packages["$package"]=1
+done < "$faulty_packages_file"
+
+# Compare the packages and print the faulty ones
+for package in "${all_packages[@]}"; do
+    if [[ ${faulty_packages["$package"]} == 1 ]]; then
+        echo "Remove faulty package: $package"
+    fi
+done
