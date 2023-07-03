765e54be771caddff5803ae11dd4302f95056da0
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-03 14:24:33 +1000

    Update removepackages.sh

--- removepackages.sh
+++ removepackages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/usernamegth/modulescybertaipan/main/packages.txt && chmod 777 packages.txt
-dpkg --get-selections > installedpackages.txt
+dpkg -l | awk '/^ii/ {print $2}' > installedpackages.txt
 sort installedpackages.txt && sed -i 's/install/ /g' installedpackages.txt
 diff installedpackages.txt packages.txt 
 sed -i 's/ //g' installedpackages.txt; sed -i 's/ //g' packages.txt
