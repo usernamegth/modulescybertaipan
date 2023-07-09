ad6dd907fd02a03263ffbb9c02473158340831d5
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-09 18:57:18 +1000

    Create adduser.sh

---
+++ adduser.sh
@@ -0,0 +1,8 @@
+awk -F: '$3>=1000{print $1}' /etc/passwd >> currentusers.txt
+sort currentusers.txt | sort users.txt
+diff --ignore-all-space currentusers.txt users.txt > diff.txt
+sed '/>/!d' diff.txt | sed 's/>//g' diff.txt | sed 's/ //g' diff.txt
+for i in $(cat diff.txt); do
+adduser $i
+done 
+
