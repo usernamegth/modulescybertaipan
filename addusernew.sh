9339aeff447eb9420aed4c8e8482f708bc501d73
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-10 17:51:28 +1000

    Update addusernew.sh

--- addusernew.sh
+++ addusernew.sh
@@ -2,4 +2,11 @@ rm -f currentusers.txt | rm -f diff.txt | rm -f diff2.txt
 awk -F: '$3>=1000{print $1}' /etc/passwd >> currentusers.txt
 diff --ignore-all-space currentusers.txt users.txt >> diff.txt  
 sed '/>/!d;s/>//g;s/ //g' diff.txt >> diff2.txt 
+for i in $(cat diff2.txt); do 
+  useradd $i
+done
+sed '/</!d;s/<//g;s/ //g' diff.txt >> diff3.txt
+for i in $(cat diff3.txt); do 
+  userdel $i
+done 
 
