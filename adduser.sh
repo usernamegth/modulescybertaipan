f1ac1a52259889ddc72c5d2ede3293ae81a71984
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-10 12:23:10 +1000

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,3 +1,4 @@
+rm currentusers.txt | rm diff.txt 
 awk -F: '$3>=1000{print $1}' /etc/passwd >> currentusers.txt
 sort currentusers.txt | sort users.txt
 diff --ignore-all-space currentusers.txt users.txt > diff.txt
@@ -6,7 +7,7 @@ echo "Users that will be added:"
 for i in $(cat diff.txt); do
 echo $i 
 done
-read -p "Delete users? [y/n]" yninput
+read -p "Add users? [y/n]" yninput
 
 if [$yninput == 'y']; then 
 for i in $(cat diff.txt); do
