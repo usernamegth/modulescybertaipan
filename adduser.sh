3d2be6d889b5fd950db839b88c7eaa2e5bc68a60
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-10 12:45:44 +1000

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,16 +1,17 @@
-rm -f currentusers.txt | rm -f diff.txt
+rm -f currentusers.txt | rm -f diff.txt | rm -f diff2.txt 
 awk -F: '$3>=1000{print $1}' /etc/passwd >> currentusers.txt
 sort currentusers.txt | sort users.txt
 diff --ignore-all-space currentusers.txt users.txt > diff.txt| chmod a+wr diff.txt 
-sed --silent '/>/!d' diff.txt >> diff.txt | sed --silent 's/>//g' diff.txt >> diff.txt | sed --silent 's/ //g' diff.txt >> diff.txt
+sed '/>/!d';'s/>//g';'s/ //g' diff.txt >> diff2.txt 
 echo "Users that will be added:"
 for i in $(cat diff.txt); do
-echo $i 
+  echo $i 
 done
 read -p "Add users? [y/n]" yninput
 
-if [$yninput == 'y']; then 
-for i in $(cat diff.txt); do
-echo 'CyberTaipan123!'| adduser $i
-done 
+if ["$yninput" == 'y']; then 
+  for i in $(cat diff.txt); do
+    echo 'CyberTaipan123!'| adduser $i
+  done 
+fi
 
