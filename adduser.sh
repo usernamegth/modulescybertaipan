08a7f7c532e16e1fbfe6befbcf5104e2b4674ce9
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-10 12:16:23 +1000

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -2,6 +2,13 @@ awk -F: '$3>=1000{print $1}' /etc/passwd >> currentusers.txt
 sort currentusers.txt | sort users.txt
 diff --ignore-all-space currentusers.txt users.txt > diff.txt
 sed '/>/!d' diff.txt | sed 's/>//g' diff.txt | sed 's/ //g' diff.txt
+echo "Users that will be added:"
+for i in $(cat diff.txt); do
+echo $i 
+done
+read -p "Delete users? [y/n]" yninput
+
+if [$yninput == 'y']; then 
 for i in $(cat diff.txt); do
 echo 'CyberTaipan123!'| adduser $i
 done 
