e16e352d6f370c5700f14c1d8240102d26d410b4
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-10 18:11:12 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,3 +1,5 @@
+# SECTION 1 
+# Deleting and adding users
 rm -f currentusers.txt | rm -f diff.txt | rm -f diff2.txt 
 awk -F: '$3>=1000{print $1}' /etc/passwd >> currentusers.txt
 diff --ignore-all-space currentusers.txt users.txt >> diff.txt  
@@ -9,4 +11,14 @@ sed '/</!d;s/<//g;s/ //g' diff.txt >> diff3.txt
 for i in $(cat diff3.txt); do 
   userdel $i
 done 
+# SECTION 2
+# Changing users passwords
+awk -F: '$3>=1000{print $1}' /etc/passwd >> newcurrentusers.txt
+sed '/ubuntu/!d' newcurrentusers.txt 
+password = "CyberTaipan123!"
+for i in $(cat newcurrentusers.txt); do 
+echo '$i:$password' | sudo chpasswd 
+
+
+
 
