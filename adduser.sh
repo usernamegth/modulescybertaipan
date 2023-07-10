5afeb5ced3dc358957ababf2ceb8abc2bde63ecd
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-10 12:47:36 +1000

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,7 +1,7 @@
 rm -f currentusers.txt | rm -f diff.txt | rm -f diff2.txt 
 awk -F: '$3>=1000{print $1}' /etc/passwd >> currentusers.txt
 sort currentusers.txt | sort users.txt
-diff --ignore-all-space currentusers.txt users.txt > diff.txt| chmod a+wr diff.txt 
+diff --ignore-all-space currentusers.txt users.txt > diff.txt | chmod a+wr diff.txt 
 sed '/>/!d';'s/>//g';'s/ //g' diff.txt >> diff2.txt 
 echo "Users that will be added:"
 for i in $(cat diff.txt); do
