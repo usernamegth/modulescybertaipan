91215e0833116c8f7eb4763285b16791ee94f307
Richard Ji <102701271+richardji1@users.noreply.github.com>
2023-07-11 14:31:06 +1000

    Update broadcast.sh

--- broadcast.sh
+++ broadcast.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
 
+# Execute your script and capture output and errors
+output=$(script.sh 2>&1)
 
-script.sh 2>&1 | tee script_output.log | curl -X POST -H "Content-Type: application/json" -d '{"content": "```'"$(cat)"'```"}' https://discord.com/api/webhooks/1128179682962051154/RwyOXVetShqlROIJ4kfwv3ynPGMt6jRc7I453Q6zdt5YOFK-LGpWM6EhAESKGRGgOouJ
+# Save output to a log file
+echo "$output" | tee script_output.log
+
+# Define the Discord webhook URL
+discord_webhook_url="https://discord.com/api/webhooks/1128179682962051154/RwyOXVetShqlROIJ4kfwv3ynPGMt6jRc7I453Q6zdt5YOFK-LGpWM6EhAESKGRGgOouJ"
+
+# Send output to Discord channel
+curl -X POST -H "Content-Type: application/json" -d '{"content": "```'"$output"'```"}' "$discord_webhook_url"
