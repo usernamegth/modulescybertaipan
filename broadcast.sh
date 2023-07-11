2d8d3b844213f55b1478fe8b54012c63cb81826a
Richard Ji <102701271+richardji1@users.noreply.github.com>
2023-07-11 14:30:10 +1000

    Update broadcast.sh

--- broadcast.sh
+++ broadcast.sh
@@ -1,3 +1,3 @@
 
 
-script.sh 2>&1 | tee script_output.log | curl -X POST -H "Content-Type: application/json" -d '{"content": "```'"$(cat)"'```"}' <DISCORD_WEBHOOK_URL>
+script.sh 2>&1 | tee script_output.log | curl -X POST -H "Content-Type: application/json" -d '{"content": "```'"$(cat)"'```"}' https://discord.com/api/webhooks/1128179682962051154/RwyOXVetShqlROIJ4kfwv3ynPGMt6jRc7I453Q6zdt5YOFK-LGpWM6EhAESKGRGgOouJ
