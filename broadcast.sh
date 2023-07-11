c3af72b7c81729693d3639ace79c2680eed598ed
Richard Ji <102701271+richardji1@users.noreply.github.com>
2023-07-11 15:19:37 +1000

    Update broadcast.sh

--- broadcast.sh
+++ broadcast.sh
@@ -8,7 +8,7 @@ while [ -e "$filename" ]; do
   filename="script_output_$counter.log"
 done
 
-output=$(user.sh 2>&1)
+output=$(chmod +x user.sh 2>&1)
 echo "$output" | tee "$filename"
 
 echo "Log file created: $filename"
