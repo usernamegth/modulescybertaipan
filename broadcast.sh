88509f4b6a8d85eda8a386c4b15bb182c4a563d4
Richard Ji <102701271+richardji1@users.noreply.github.com>
2023-07-11 14:48:27 +1000

    Update broadcast.sh to include webhook url

--- broadcast.sh
+++ broadcast.sh
@@ -11,7 +11,7 @@ done
 output=$(script.sh 2>&1)
 echo "$output" | tee "$filename"
 
-discord_webhook_url="https://discord.com/api/webhooks/your_webhook_url"
+discord_webhook_url="https://discord.com/api/webhooks/1128179682962051154/RwyOXVetShqlROIJ4kfwv3ynPGMt6jRc7I453Q6zdt5YOFK-LGpWM6EhAESKGRGgOouJ"
 
 curl -X POST -H "Content-Type: application/json" -d '{"content": "```'"$output"'```"}' "$discord_webhook_url"
 
