da5e114aed898fea32d9a4abb509d748e2673bf5
Richard Ji <102701271+richardji1@users.noreply.github.com>
2023-07-11 16:20:18 +1000

    Update broadcast.sh

--- broadcast.sh
+++ broadcast.sh
@@ -14,6 +14,8 @@ echo "Log file created: $filename"
 
 discord_webhook_url="https://discord.com/api/webhooks/1128179682962051154/RwyOXVetShqlROIJ4kfwv3ynPGMt6jRc7I453Q6zdt5YOFK-LGpWM6EhAESKGRGgOouJ"
 
-curl -X POST -H "Content-Type: application/json" -d '{"content": "```'"$output"'```"}' "$discord_webhook_url"
+# Properly format the JSON payload
+json_payload=$(jq -n --arg output "$output" '{"content": $output}')
 
-
+# Send the JSON payload using curl
+curl -X POST -H "Content-Type: application/json" -d "$json_payload" "$discord_webhook_url"
