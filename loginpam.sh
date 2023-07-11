377367ee2c691c773febf5dc0b243611a65bc5e9
usernamegth <112678550+usernamegth@users.noreply.github.com>
2023-07-11 10:27:00 +1000

    Update loginpam.sh

--- loginpam.sh
+++ loginpam.sh
@@ -1,3 +1,7 @@
-w="30";file="/etc/login.defs"
+w="30";file="/etc/login.defs";f="7"
 l=$(grep -n "PASS_MAX_DAYS" "$file" | cut -d':' -f1 | tail -n 1)
 sed -i "${l}s/[0-9]\+/$w/" "$file"
+echo "PASS_MAX_DAYS changed to 30."
+h=$(grep -n "PASS_MIN_DAYS" "$file" | cut -d':' -f1 | tail -n 1)
+sed -i "${h}s/[0-9]\+/$f/" "$file"
+echo "PASS_MIN_DAYS changed to 7."
