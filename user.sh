e5eb02f060328f9628dfe7b6346315789f1a56b7
Richard Ji <102701271+richardji1@users.noreply.github.com>
2023-07-11 16:02:50 +1000

    Update user.sh (unreliable, go to previous if doesn't work)

--- user.sh
+++ user.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+
 # SECTION 1 
 # Deleting and adding users
 rm -f currentusers.txt | rm -f diff.txt | rm -f diff2.txt 
 awk -F: '$3>=1000{print $1}' /etc/passwd >> currentusers.txt
 diff --ignore-all-space currentusers.txt users.txt >> diff.txt  
 sed '/>/!d;s/>//g;s/ //g' diff.txt >> diff2.txt 
-for i in $(cat diff2.txt); do 
-  useradd $i
-done
+while IFS= read -r i; do 
+  useradd "$i"
+done < diff2.txt
 sed '/</!d;s/<//g;s/ //g' diff.txt >> diff3.txt
-for i in $(cat diff3.txt); do 
-  userdel $i
-done 
+while IFS= read -r i; do 
+  userdel "$i"
+done < diff3.txt
 
 # SECTION 2
 # Changing users passwords
@@ -19,9 +20,9 @@ awk -F: '$3>=1000{print $1}' /etc/passwd >> newcurrentusers.txt
 sed -i '/ubuntu/d' newcurrentusers.txt
 
 password="CyberTaipan123!"
-for i in $(cat newcurrentusers.txt); do 
+while IFS= read -r i; do 
   echo "$i:$password" | sudo chpasswd 
-done
+done < newcurrentusers.txt
 
 
 
